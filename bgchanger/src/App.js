47931ede794cdfaf1d812bce7482d8721265430e
kartikay varun <varunkartikay0325@gmail.com>
2024-08-09 19:39:56 +0530

    bgchanger completed

--- bgchanger/src/App.js
+++ bgchanger/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[color , setColor] = useState("red")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className='hero'>
+  <div className='container' style={{backgroundColor : color}} >
+    <h1>BG Changer</h1>
+  </div>
+  <div className='btns'>
+    <div className='bar'>
+      <button className='colrbtn red' onClick={() => setColor("red")}>Red</button>
+      <button className='colrbtn blue' onClick={() => setColor("blue")}>Blue</button>
+      <button className='colrbtn green' onClick={() => setColor("green")}>Green</button>
+      <button className='colrbtn violet' onClick={() => setColor("violet")}>Violet</button>
+      <button className='colrbtn yellow' onClick={() => setColor("yellow")}>Yellow</button>
+      <button className='colrbtn pink' onClick={() => setColor("pink")}>Pink</button>
     </div>
+  </div>
+</div>
   );
 }
 
