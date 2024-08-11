01e1f753f72a543f615b40b6c51a4b9e6abeb8ae
kartikay varun <varunkartikay0325@gmail.com>
2024-08-11 18:35:32 +0530

    Fully Completed

--- quote-generator/src/Components/RandomQuotes/RandomQuotes.jsx
+++ quote-generator/src/Components/RandomQuotes/RandomQuotes.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react'
 import "./RandomQuotes.css"
+import { TfiReload } from "react-icons/tfi";
 
 const RandomQuotes = () => {
-    
 
-const [quotes , setQuotes] = useState({
+  let quotes = [];
+
+  async function loadQuotes () {
+
+    const response = await fetch("http://type.fit/api/quotes");
+    quotes = await response.json()
+    
+  }
+    
+    const reload = () => {
+      const select = quotes[Math.floor(Math.random()* quotes.length)];
+      setQuote(select)
+    }
+
+    loadQuotes();
+
+const [quote , setQuote] = useState({
     text:"If you are good at something never do it for freee",
     author:"Joker"
 })
@@ -13,12 +29,13 @@ const [quotes , setQuotes] = useState({
   return (
     <div className='container'>
         <div className='quoteText'>
-            <div className='Text'>{quotes.text}</div>
+            <div className='Text'>{quote.text}</div>
         </div>
         <div className='line'></div>
         <div className='quoteAuthor'>
-            <div className='Author'>{quotes.author}</div>
+            <div className='Author'>{quote.author}</div>
             <div className='icon'>
+            <TfiReload  onClick={()=> reload() }/>
 
             </div>
         </div>
